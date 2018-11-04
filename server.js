0ab8fd546559f9e83d646de1e8dc695ea3ca2350
Manuel <manuel.c1992@gmail.com>
2018-11-04 01:55:14 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require('express');
-const { Client } = require('pg');
+var path = require("path");
+var bodyParser = require('body-parser');
+
+var api = require('./api.js');
 const app = express();
 const port = 3000;
-const databaseUri = 'postgres://jxhsmzbyblxtja:9cf35b7c333850010b2d50b7458746e0ddf7ebb078c76b68c4b380c24b82b0f2@ec2-184-73-222-192.compute-1.amazonaws.com:5432/de02qip1j8b2vp';
-const client = new Client({
-    connectionString: databaseUri,
-    ssl: true
-});
 
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-app.get('/', async (req, res) => {
-    await client.connect();
-    result = await client.query('SELECT * FROM db.full_view');
-    res.json(result.rows);
-});
+app.use('/node_modules', express.static(__dirname + '/node_modules/'));
+app.use('/', express.static(__dirname + '/src/'));
+app.use('/api', api);
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+  });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
